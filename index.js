d75147d12134e2dd048c49f65db5b4a81740e2e3
Ferry Ananda Febian <53357609+ferryops@users.noreply.github.com>
2025-04-07 05:35:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ async function main() {
 
     const readmeContent = `
 
+![Github Stats](https://github-readme-stats.vercel.app/api?bg_color=0000&title_color=4C71F1&text_color=8A919F&line_height=24&border_color=8884&username=ferryops&hide=contribs&show_icons=true&count_private=true&theme=vue)
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?bg_color=0000&title_color=4C71F1&text_color=8A919F&card_width=240&border_color=8884&username=ferryops&layout=compact&theme=vue)
+
 Saya tidak cuma nulis tentang teknologi, tapi juga nulis kode yang bagus dan minim bug, update terakhir: ${formattedDate}
 | Artikel Terbaru | Projects Terbaru |
 |--|--|
